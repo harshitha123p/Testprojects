72182732527d70bdc8203354c80b6e92701773de
HARSHITHA\DCL21 <harshithap906@gmail.com>
2022-05-24 21:46:31 +0530

    no message

---
+++ TrainingPrograms/src/com/foreachloop.java
@@ -0,0 +1,14 @@
+package com;
+
+public class foreachloop {
+
+	public static void main(String args[])
+	{
+		
+		int s[]= {18,20,21,34,45};
+		for (int i:s)
+		{
+		System.out.println(i);
+	}
+	}
+}
